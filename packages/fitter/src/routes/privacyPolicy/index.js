ffe8ffceab1e296e8464b98c537f3cd8f6c7d5b6
Attila Bartha <Attila.Bartha@ibm.com>
2021-01-29 16:29:12 +0100

    add empty privacy policy

---
+++ packages/fitter/src/routes/privacyPolicy/index.js
@@ -0,0 +1,15 @@
+import { h } from 'preact';
+
+import Page from '../../components/Page/Page';
+import Title from '../../components/Page/Title';
+
+const Home = () => (
+  <>
+    <Page>
+      <Title>Privacy Policy</Title>
+      <p></p>
+    </Page>
+  </>
+);
+
+export default Home;
