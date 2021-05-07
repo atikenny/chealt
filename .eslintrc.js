1e3224049e065d2f69968dacb413d048c4decd30
Attila Bartha <atikenny@gmail.com>
2021-05-07 13:28:46 +0200

    install web dev server and create initial index

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,32 +4,13 @@ const config = {
   ...eslintConfig,
   overrides: [
     {
-      files: ['packages/fitter/**/*.js'],
-      extends: ['plugin:react/recommended'],
-      plugins: ['react', 'react-hooks'],
-      settings: {
-        react: {
-          pragma: 'h',
-          version: 'detect'
-        }
-      },
-      rules: {
-        'react/no-unknown-property': ['error', { ignore: ['class'] }],
-        'react/prop-types': 'off'
-      },
-      parserOptions: {
-        babelOptions: {
-          configFile: './packages/fitter/.babelrc'
-        }
-      }
-    },
-    {
       files: [
+        'packages/check/**/*.js',
+        'packages/dashboard/**/*.js',
         'packages/jest-puppeteer-env/**/*.js',
         'packages/jest-puppeteer-env-example/**/*.js',
         'packages/mocker/**/*.js',
         'packages/mocker/**/*.cjs',
-        'packages/check/**/*.js',
         './.eslintrc.js'
       ],
       parserOptions: {
