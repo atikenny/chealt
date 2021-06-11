2b5ba9c2fadc1f8c509e882009520671459b0b28
Attila Bartha <atikenny@gmail.com>
2021-06-11 15:10:31 +0200

    remove URL from header

--- packages/dashboard/public/components/Header.js
+++ packages/dashboard/public/components/Header.js
@@ -1,22 +1,13 @@
-import { useLocation } from 'preact-iso';
 import Link from './Link';
 
-const Header = () => {
-  const { url } = useLocation();
-
-  return (
-    <header>
-      <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/error">Error</Link>
-      </nav>
-      <label>
-        URL:
-        <input readonly value={url} ref={(c) => c && (c.size = c.value.length)} />
-      </label>
-    </header>
-  );
-};
+const Header = () => (
+  <header>
+    <nav>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/error">Error</Link>
+    </nav>
+  </header>
+);
 
 export default Header;
